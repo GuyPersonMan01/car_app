133261cb5ce958dfa70510580199905b77253584
SamSri01 <109153482+GuyPersonMan01@users.noreply.github.com>
2023-11-28 20:48:12 -0500

    Add files via upload

---
+++ home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def app():
+	st.header('Car Price Prediction')
+	st.text('''
+		This web app allows a user to predict the prices of a car
+		based on their engine size, horsepower,
+		dimensions and the drive wheel type parameters.''')
+
