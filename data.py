2f970a11e3c758776d7fec70208c788363db25da
SamSri01 <109153482+GuyPersonMan01@users.noreply.github.com>
2023-11-28 20:52:17 -0500

    Update data.py

--- data.py
+++ data.py
@@ -2,20 +2,17 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+# Define a function 'app()' which accepts 'car_df' as an input.
 def app(car_df):
-    # Displaying orginal dataset
     st.header("View Data")
     # Add an expander and display the dataset as a static table within the expander.
     with st.beta_expander("View Dataset"):
         st.table(car_df)
 
-    # Display descriptive statistics.
     st.subheader("Columns Description:")
     if st.checkbox("Show summary"):
         st.table(car_df.describe())
 
-    # ADD NEW CODE FROM HERE
-    # Add a subheader and create three columns. Store the columns in two separate variables.
     beta_col1, beta_col2, beta_col3 = st.beta_columns(3)
 
     # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
